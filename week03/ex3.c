4c413bfdf5f76a1bd4c9ab9b0a3cc853456f4d93
lvjonok <kozlov.l.a@mail.ru>
2021-09-07 20:00:51 +0300

    fixed solution with list for deletion of first element

--- week03/ex3.c
+++ week03/ex3.c
@@ -9,7 +9,7 @@ struct node {
 
 // function returns pointer to newly created node with given data
 struct node* create_node(int data) {
-  struct node* new_node = malloc(sizeof(struct node));
+  struct node* new_node = (struct node*)malloc(sizeof(struct node));
   new_node->next = 0;
   new_node->data = data;
   return new_node;
@@ -33,14 +33,23 @@ struct node* insert_node(struct node* predecessor, struct node* new_node) {
 }
 
 // function removes node given by pointer from list by searching
-int delete_node(struct node* head, struct node* to_delete) {
-  while (head->next != to_delete && head->next != 0) {
+struct node* delete_node(struct node* head, struct node* to_delete) {
+  if (head == 0) return head;
+
+  if (head == to_delete) {
+    struct node* temp = head;
     head = head->next;
+    free(temp);
+    return head;
+  } else {
+    while (head->next != to_delete && head->next != 0) {
+      head = head->next;
+    }
+    head->next = to_delete->next;
+    int to_return = to_delete->data;
+    free(to_delete);
+    return head;
   }
-  head->next = to_delete->next;
-  int to_return = to_delete->data;
-  free(to_delete);
-  return to_return;
 }
 
 int main() {
@@ -53,12 +62,20 @@ int main() {
   struct node* el4 = create_node(100000);
 
   insert_node(linked_list, el);  // 1 -> 10
-  insert_node(el, el1);          // 1 -> 10 -> 100
-  insert_node(el, el2);          // 1 -> 10 -> 1000 -> 100
-  insert_node(el2, el3);         // 1 -> 10 -> 1000 -> 10000 -> 100
-  insert_node(el2, el4);         // 1 -> 10 -> 1000 -> 100000 -> 10000 -> 100
+  print_list(linked_list);
+  insert_node(el, el1);  // 1 -> 10 -> 100
+  print_list(linked_list);
+  insert_node(el, el2);  // 1 -> 10 -> 1000 -> 100
+  print_list(linked_list);
+  insert_node(el2, el3);  // 1 -> 10 -> 1000 -> 10000 -> 100
+  print_list(linked_list);
+  insert_node(el2, el4);  // 1 -> 10 -> 1000 -> 100000 -> 10000 -> 100
+  print_list(linked_list);
 
-  delete_node(linked_list, el);  // 1 -> 1000 -> 10000 -> 100
+  linked_list =
+      delete_node(linked_list, el);  // 1 -> 1000 -> 100000 -> 10000 -> 100
+  print_list(linked_list);
+  linked_list = delete_node(linked_list, linked_list);
 
   print_list(linked_list);
 }
