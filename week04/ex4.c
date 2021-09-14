caa4d2ca9e6a939ce3969cc730d9be554d3e4cc9
lvjonok <kozlov.l.a@mail.ru>
2021-09-14 23:13:50 +0300

    last changes for last task

--- week04/ex4.c
+++ week04/ex4.c
@@ -1,21 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define EXIT_CODE "giancarlo"
 
 int main() {
   printf("This is simple shell, white `%s` to stop its execution\n", EXIT_CODE);
-  char* cmd = malloc(sizeof(char) * 256);
   while (1) {
-    free(cmd);
-    cmd = malloc(sizeof(char) * 256);
-    fgets(cmd, 256, stdin);
-    if (!strncmp(cmd, EXIT_CODE, strlen(EXIT_CODE))) break;
-    if (fork() == 0) {
-      system(cmd);
+    printf("%s %s >", getenv("USER"), getenv("PWD"));
+    char *tokens[10];
+    char *raw_cmd = malloc(sizeof(char) * 256);
+    fgets(raw_cmd, 256, stdin);
+
+    if (!strncmp(raw_cmd, EXIT_CODE, strlen(EXIT_CODE))) break;
+    int background_process = 0;
+
+    char *token;
+    int token_idx = 0;
+    token = strtok(raw_cmd, " \n");
+    while (token != 0) {
+      // symbol for background process will be either in front or at the end of
+      // token
+
+      background_process = (token[0] == '&' || token[strlen(token) - 1] == '&')
+                               ? 1
+                               : background_process;
+
+      tokens[token_idx++] = token;
+      token = strtok(NULL, " \n");
+    }
+    tokens[token_idx] = NULL;
+
+    pid_t pid = fork();
+    if (pid == 0) {
+      // child process
+      if (background_process) {
+        // year, it is a cheat thing
+        daemon(0, 0);
+      }
+      execvp(tokens[0], tokens);
       exit(0);
+    } else {
+      int status;
+      waitpid(pid, &status, 0);
     }
   }
   return 0;
